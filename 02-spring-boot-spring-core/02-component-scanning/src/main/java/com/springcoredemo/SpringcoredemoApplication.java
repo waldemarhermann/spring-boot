f309eeacd24ee44a5f0e980f28f973d32fec846a
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-01-28 12:49:16 +0100

    typo

--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/springcoredemo/SpringcoredemoApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /* @SpringBootApplication (
-		scanBasePackages = {"com.springcoredemo",
-							"com.util"})
+		scanBasePackages = {"com.springcoredemo","com.util"})
  */
 
 @SpringBootApplication
