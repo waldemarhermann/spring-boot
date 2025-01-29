cddb6e6d1489061c71c43a3cb8bef7088c0abfb9
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-01-29 14:37:11 +0100

    add new folder - primary

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice every day Tennis!";
+    }
+
+}
---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.springcoredemo.rest;
+
+import com.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
