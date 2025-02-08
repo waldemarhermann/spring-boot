2b0a7f1f3eac20020a445dad7d29b13dcd259e38
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-02-08 16:24:00 +0100

    add StudentDAO-Interface & StudentDAOImpl class

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/cruddemo/dao/StudentDAOImpl.java
@@ -1,4 +1,31 @@
 package com.cruddemo.dao;
 
-public class StudentDAOImpl {
+import com.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+
+    // define fields for entity manager
+    private EntityManager entityManager;
+
+    @Autowired
+    // inject entity manager using constructor injection
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // implement save method
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
 }
+
+
+
+
