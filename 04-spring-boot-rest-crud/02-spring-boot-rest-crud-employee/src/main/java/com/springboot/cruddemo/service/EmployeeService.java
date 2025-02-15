cd7a133492e7b913263a6ae9ea49aac1f3d12538
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-02-15 21:41:44 +0100

    add Services

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.springboot.cruddemo.service;
+
+import com.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+}
---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.springboot.cruddemo.service;
+
+import com.springboot.cruddemo.dao.EmployeeDAO;
+import com.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+}
