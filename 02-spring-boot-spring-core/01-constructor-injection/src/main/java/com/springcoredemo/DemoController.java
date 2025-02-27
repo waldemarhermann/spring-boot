a76747dda03a354f6997bb30487c4090df65daaa
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-02-27 19:05:33 +0100

    .

--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/springcoredemo/DemoController.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/springcoredemo/DemoController.java
@@ -4,19 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class DemoController {
 
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach theCoach) {
-        myCoach = theCoach;
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
     }
 
-    @GetMapping("/dailyworkout")
+    @GetMapping("/getworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
+
 }
