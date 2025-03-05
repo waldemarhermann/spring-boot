e65aff5fd34cd07e27998689e6360fed85aa8e49
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-03-05 11:33:42 +0100

    add mvc validation rules

--- 06-spring-boot-mvc/validationdemo/src/main/java/com/springdemo/mvc/Customer.java
+++ 06-spring-boot-mvc/validationdemo/src/main/java/com/springdemo/mvc/Customer.java
@@ -1,7 +1,6 @@
 package com.springdemo.mvc;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -11,6 +10,29 @@ public class Customer {
     @Size(min=1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
+    @Min(value=0, message="must be greater than or equal to 0")
+    @Max(value=10, message ="must be less or equal to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public Customer() {
     }
