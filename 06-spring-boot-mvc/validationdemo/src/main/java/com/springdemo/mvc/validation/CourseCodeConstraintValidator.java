65b550d9b5131174d5141820a0e798bb9e7b4c96
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-03-06 21:19:34 +0100

    complete validation

---
+++ 06-spring-boot-mvc/validationdemo/src/main/java/com/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,29 @@
+package com.springdemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode courseCode) {
+        ConstraintValidator.super.initialize(courseCode);
+        coursePrefix = courseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCode, ConstraintValidatorContext constraintValidatorContext) {
+
+        boolean result;
+
+        if (theCode != null) {
+            result = theCode.startsWith(coursePrefix);
+        } else {
+            result = true;
+        }
+
+        return result;
+    }
+}
