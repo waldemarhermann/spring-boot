38bb86e252adf929ec1e8b03ac6d4785f7d28d16
waldemarhermann <hermann.waldemar1995@gmail.com>
2025-03-09 13:42:20 +0100

    add Function - add employee

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -30,15 +32,23 @@ public class EmployeeController {
         // add list to model
         model.addAttribute("employees", listEmployees);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
 
-    @GetMapping("/employees/showFormForAdd")
+    @GetMapping("/showFormForAdd")
     public String showFormForAdd(Model model) {
-
 
         model.addAttribute("employee", new Employee());
 
         return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+
+        employeeService.save(employee);
+
+        // redirect --> GET-Methode wird angewandt
+        return "redirect:/employees/list";
     }
 }
